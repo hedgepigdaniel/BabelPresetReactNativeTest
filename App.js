0f130981f6fc52e9f1bb7d28941b2bedf28e7d61
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2018-01-22 11:05:52 +1100

    Add example of an undefined import that should be caught at compilation time

--- App.js
+++ App.js
@@ -12,6 +12,8 @@ import {
   View
 } from 'react-native';
 
+import { IMPORTED_THING } from './helper.js';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -24,13 +26,13 @@ export default class App extends Component<{}> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+          {
+            IMPORTED_THING === undefined ? (
+              'FAIL! This undefined import should have been caught during compilation!'
+            ) : (
+              `SUCCESS! IMPORTED_THING = "${IMPORTED_THING}"`
+            )
+          }
         </Text>
       </View>
     );
---
+++ helper.js
@@ -0,0 +1,1 @@
+export const MISSPELLED_THING = 'Tomatoes :}';
